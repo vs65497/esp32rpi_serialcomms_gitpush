4124a7ef9c799856389f31b32ae6609f780cb0c1
vs65497 <von.developer@gmail.com>
2023-06-27 14:31:30 -0700

    Add files via upload

--- collect/collect.ino
+++ collect/collect.ino
@@ -15,86 +15,6 @@ Adafruit_TSL2591 tsl = Adafruit_TSL2591(2591);
 
 const unsigned int MAX_MESSAGE_LENGTH = 12;
 
-/**************************************************************************/
-/*
-    Displays some basic information on this sensor from the unified
-    sensor API sensor_t type (see Adafruit_Sensor for more information)
-*/
-/**************************************************************************/
-void displaySensorDetails(void)
-{
-  sensor_t sensor;
-  tsl.getSensor(&sensor);
-  Serial.println(F("------------------------------------"));
-  Serial.print  (F("Sensor:       ")); Serial.println(sensor.name);
-  Serial.print  (F("Driver Ver:   ")); Serial.println(sensor.version);
-  Serial.print  (F("Unique ID:    ")); Serial.println(sensor.sensor_id);
-  Serial.print  (F("Max Value:    ")); Serial.print(sensor.max_value); Serial.println(F(" lux"));
-  Serial.print  (F("Min Value:    ")); Serial.print(sensor.min_value); Serial.println(F(" lux"));
-  Serial.print  (F("Resolution:   ")); Serial.print(sensor.resolution, 4); Serial.println(F(" lux"));  
-  Serial.println(F("------------------------------------"));
-  Serial.println(F(""));
-  delay(500);
-}
-
-/**************************************************************************/
-/*
-    Configures the gain and integration time for the TSL2591
-*/
-/**************************************************************************/
-void configureSensor(void)
-{
-  // You can change the gain on the fly, to adapt to brighter/dimmer light situations
-  //tsl.setGain(TSL2591_GAIN_LOW);    // 1x gain (bright light)
-  tsl.setGain(TSL2591_GAIN_MED);      // 25x gain
-  //tsl.setGain(TSL2591_GAIN_HIGH);   // 428x gain
-  
-  // Changing the integration time gives you a longer time over which to sense light
-  // longer timelines are slower, but are good in very low light situtations!
-  //tsl.setTiming(TSL2591_INTEGRATIONTIME_100MS);  // shortest integration time (bright light)
-  // tsl.setTiming(TSL2591_INTEGRATIONTIME_200MS);
-  tsl.setTiming(TSL2591_INTEGRATIONTIME_300MS);
-  // tsl.setTiming(TSL2591_INTEGRATIONTIME_400MS);
-  // tsl.setTiming(TSL2591_INTEGRATIONTIME_500MS);
-  // tsl.setTiming(TSL2591_INTEGRATIONTIME_600MS);  // longest integration time (dim light)
-
-  /* Display the gain and integration time for reference sake */  
-  Serial.println(F("------------------------------------"));
-  Serial.print  (F("Gain:         "));
-  tsl2591Gain_t gain = tsl.getGain();
-  switch(gain)
-  {
-    case TSL2591_GAIN_LOW:
-      Serial.println(F("1x (Low)"));
-      break;
-    case TSL2591_GAIN_MED:
-      Serial.println(F("25x (Medium)"));
-      break;
-    case TSL2591_GAIN_HIGH:
-      Serial.println(F("428x (High)"));
-      break;
-    case TSL2591_GAIN_MAX:
-      Serial.println(F("9876x (Max)"));
-      break;
-  }
-  Serial.print  (F("Timing:       "));
-  Serial.print((tsl.getTiming() + 1) * 100, DEC); 
-  Serial.println(F(" ms"));
-  Serial.println(F("------------------------------------"));
-  Serial.println(F(""));
-}
-
-/* return absolute humidity [mg/m^3] with approximation formula
-* @param temperature [°C]
-* @param humidity [%RH]
-*/
-uint32_t getAbsoluteHumidity(float temperature, float humidity) {
-    // approximation formula from Sensirion SGP30 Driver Integration chapter 3.15
-    const float absoluteHumidity = 216.7f * ((humidity / 100.0f) * 6.112f * exp((17.62f * temperature) / (243.12f + temperature)) / (273.15f + temperature)); // [g/m^3]
-    const uint32_t absoluteHumidityScaled = static_cast<uint32_t>(1000.0f * absoluteHumidity); // [mg/m^3]
-    return absoluteHumidityScaled;
-}
-
 bool handshake() {
  int counter = 0;
  int timeout = 5; // timeout after 5 seconds
@@ -151,135 +71,52 @@ bool handshake() {
  }
 }
 
-/**************************************************************************/
-/*
-    Shows how to perform a basic read on visible, full spectrum or
-    infrared light (returns raw 16-bit ADC values)
-*/
-/**************************************************************************/
-void simpleRead(void)
-{
-  // Simple data read example. Just read the infrared, fullspecrtrum diode 
-  // or 'visible' (difference between the two) channels.
-  // This can take 100-600 milliseconds! Uncomment whichever of the following you want to read
-  uint16_t x = tsl.getLuminosity(TSL2591_VISIBLE);
-  //uint16_t x = tsl.getLuminosity(TSL2591_FULLSPECTRUM);
-  //uint16_t x = tsl.getLuminosity(TSL2591_INFRARED);
-
-  Serial.print(F("[ ")); Serial.print(millis()); Serial.print(F(" ms ] "));
-  Serial.print(F("Luminosity: "));
-  Serial.println(x, DEC);
-}
-
-/**************************************************************************/
-/*
-    Show how to read IR and Full Spectrum at once and convert to lux
-*/
-/**************************************************************************/
-void advancedRead(void)
-{
-  // More advanced data read example. Read 32 bits with top 16 bits IR, bottom 16 bits full spectrum
-  // That way you can do whatever math and comparisons you want!
-  uint32_t lum = tsl.getFullLuminosity();
-  uint16_t ir, full;
-  ir = lum >> 16;
-  full = lum & 0xFFFF;
-  Serial.print(F("[ ")); Serial.print(millis()); Serial.print(F(" ms ] "));
-  Serial.print(F("IR: ")); Serial.print(ir);  Serial.print(F("  "));
-  Serial.print(F("Full: ")); Serial.print(full); Serial.print(F("  "));
-  Serial.print(F("Visible: ")); Serial.print(full - ir); Serial.print(F("  "));
-  Serial.print(F("Lux: ")); Serial.println(tsl.calculateLux(full, ir), 6);
-}
-
-/**************************************************************************/
-/*
-    Performs a read using the Adafruit Unified Sensor API.
-*/
-/**************************************************************************/
-void unifiedSensorAPIRead(void)
-{
-  /* Get a new sensor event */ 
-  sensors_event_t event;
-  tsl.getEvent(&event);
- 
-  /* Display the results (light is measured in lux) */
-  Serial.print(F("[ ")); Serial.print(event.timestamp); Serial.print(F(" ms ] "));
-  if ((event.light == 0) |
-      (event.light > 4294966000.0) | 
-      (event.light <-4294966000.0))
-  {
-    /* If event.light = 0 lux the sensor is probably saturated */
-    /* and no reliable data could be generated! */
-    /* if event.light is +/- 4294967040 there was a float over/underflow */
-    Serial.println(F("Invalid data (adjust gain or timing)"));
-  }
-  else
-  {
-    Serial.print(event.light); Serial.println(F(" lux"));
-  }
-}
-
 void collect() {
 
   int counter = 0;
   bool canCollect = true;
   while (canCollect) {
-      //Check to see if anything is available in the serial receive buffer
 
-    // BME680 ********************
-    if (! bme.performReading()) {
-      Serial.println("Failed to perform reading :(");
-      return;
-    }
-    Serial.print("Temperature = ");
-    Serial.print(bme.temperature);
-    Serial.println(" *C");
+    // BME680
+    // Temperature *C
+    Serial.print(bme.temperature); Serial.print(",");
+    // Pressure hPa
+    Serial.print(bme.pressure / 100.0); Serial.print(",");
+    // Humidity %
+    Serial.print(bme.humidity); Serial.print(",");
+    // Gas Resistance kOhms
+    Serial.print(bme.gas_resistance / 1000.0); Serial.print(",");
+    // Altitude m
+    Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA)); Serial.print(",");
 
-    Serial.print("Pressure = ");
-    Serial.print(bme.pressure / 100.0);
-    Serial.println(" hPa");
+    // TSL2591 LUX
+    uint32_t lum = tsl.getFullLuminosity();
+    uint16_t ir, full;
+    ir = lum >> 16;
+    full = lum & 0xFFFF;
+    // mllis ms
+    Serial.print(millis()); Serial.print(",");
+    // IR
+    Serial.print(ir); Serial.print(",");
+    // Full
+    Serial.print(full); Serial.print(",");
+    // Visible
+    Serial.print(full - ir); Serial.print(",");
+    // Lux
+    Serial.print(tsl.calculateLux(full, ir), 6); Serial.print(",");
 
-    Serial.print("Humidity = ");
-    Serial.print(bme.humidity);
-    Serial.println(" %");
+    // SGP30 
+    // TVOC ppb
+    Serial.print(sgp.TVOC); Serial.print(",");
+    // eCO2 ppm
+    Serial.print(sgp.eCO2); Serial.print(",");
+    // Raw H2
+    Serial.print(sgp.rawH2); Serial.print(",");
+    // Raw Ethanol
+    Serial.print(sgp.rawEthanol);
 
-    Serial.print("Gas = ");
-    Serial.print(bme.gas_resistance / 1000.0);
-    Serial.println(" KOhms");
-
-    Serial.print("Approx. Altitude = ");
-    Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
-    Serial.println(" m");
-
+    // New line
     Serial.println();
-    //delay(2000);
-
-    // TSL2591 LUX ********************
-    //simpleRead(); 
-    advancedRead();
-    // unifiedSensorAPIRead();
-    
-    //delay(500);
-
-    // SGP30 ********************
-    // If you have a temperature / humidity sensor, you can set the absolute humidity to enable the humditiy compensation for the air quality signals
-    //float temperature = 22.1; // [°C]
-    //float humidity = 45.2; // [%RH]
-    //sgp.setHumidity(getAbsoluteHumidity(temperature, humidity));
-
-    if (! sgp.IAQmeasure()) {
-      Serial.println("Measurement failed");
-      return;
-    }
-    Serial.print("TVOC "); Serial.print(sgp.TVOC); Serial.print(" ppb\t");
-    Serial.print("eCO2 "); Serial.print(sgp.eCO2); Serial.println(" ppm");
-
-    if (! sgp.IAQmeasureRaw()) {
-      Serial.println("Raw Measurement failed");
-      return;
-    }
-    Serial.print("Raw H2 "); Serial.print(sgp.rawH2); Serial.print(" \t");
-    Serial.print("Raw Ethanol "); Serial.print(sgp.rawEthanol); Serial.println("");
   
     delay(1000);
 
@@ -288,16 +125,9 @@ void collect() {
     }
 
     counter++;
+
     if (counter == 30) {
       counter = 0;
-
-      uint16_t TVOC_base, eCO2_base;
-      if (! sgp.getIAQBaseline(&eCO2_base, &TVOC_base)) {
-        Serial.println("Failed to get baseline readings");
-        return;
-      }
-      Serial.print("****Baseline values: eCO2: 0x"); Serial.print(eCO2_base, HEX);
-      Serial.print(" & TVOC: 0x"); Serial.println(TVOC_base, HEX);
     }
   }
 }
@@ -335,59 +165,33 @@ void setup() {
     
     Serial.println("Attempting to handshake with RPi4...");    
   }
-  Serial.println("Handshake successful!");
 
-  while (!Serial);
-  Serial.println(F("BME680 test"));
-
-  if (!bme.begin()) {
-    Serial.println("Could not find a valid BME680 sensor, check wiring!");
-    while (1);
-  }
-
-  // Set up oversampling and filter initialization
+  // Configure BME680
   bme.setTemperatureOversampling(BME680_OS_8X);
   bme.setHumidityOversampling(BME680_OS_2X);
   bme.setPressureOversampling(BME680_OS_4X);
   bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
   bme.setGasHeater(320, 150); // 320*C for 150 ms
 
-  // TSL2591 LUX ********************
-  //Serial.begin(9600);
+  // Configure TSL2591 LUX 
+  // You can change the gain on the fly, to adapt to brighter/dimmer light situations
+  //tsl.setGain(TSL2591_GAIN_LOW);    // 1x gain (bright light)
+  tsl.setGain(TSL2591_GAIN_MED);      // 25x gain
+  //tsl.setGain(TSL2591_GAIN_HIGH);   // 428x gain
   
-  Serial.println(F("Starting Adafruit TSL2591 Test!"));
-  
-  if (tsl.begin()) 
-  {
-    Serial.println(F("Found a TSL2591 sensor"));
-  } 
-  else 
-  {
-    Serial.println(F("No sensor found ... check your wiring?"));
-  }
-  /* Configure the sensor */
-  configureSensor();
+  // Changing the integration time gives you a longer time over which to sense light
+  // longer timelines are slower, but are good in very low light situtations!
+  //tsl.setTiming(TSL2591_INTEGRATIONTIME_100MS);  // shortest integration time (bright light)
+  // tsl.setTiming(TSL2591_INTEGRATIONTIME_200MS);
+  tsl.setTiming(TSL2591_INTEGRATIONTIME_300MS);
+  // tsl.setTiming(TSL2591_INTEGRATIONTIME_400MS);
+  // tsl.setTiming(TSL2591_INTEGRATIONTIME_500MS);
+  // tsl.setTiming(TSL2591_INTEGRATIONTIME_600MS);  // longest integration time (dim light)
 
-  // Now we're ready to get readings ... move on to loop()!
+  // Configure SGP30
+  // None
 
-  // SGP30 ********************
-  //Serial.begin(115200);
-  while (!Serial) { delay(10); } // Wait for serial console to open!
-
-  Serial.println("SGP30 test");
-
-  if (! sgp.begin()){
-    Serial.println("Sensor not found :(");
-    while (1);
-  }
-  Serial.print("Found SGP30 serial #");
-  Serial.print(sgp.serialnumber[0], HEX);
-  Serial.print(sgp.serialnumber[1], HEX);
-  Serial.println(sgp.serialnumber[2], HEX);
-
-  // If you have a baseline measurement from before you can assign it to start, to 'self-calibrate'
-  //sgp.setIAQBaseline(0x8E68, 0x8F41);  // Will vary for each sensor!
-
+  // Start collecting
   collect();
 
   // waiting for autonomous push to complete.
@@ -403,7 +207,3 @@ void setup() {
 void loop() {
 
 }
-
-
-
-// code from: https://www.programmingelectronics.com/serial-read/
