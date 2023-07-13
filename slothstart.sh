0354ced5e6be5f39d4474f8266aa1b689ef43105
Von: Automated Push <von.developer@gmail.com>
2023-07-13 16:55:53 -0700

    adding startup script to the repo

--- slothstart.sh
+++ slothstart.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-python3 /home/sloth/esp32rpi_serialcomms_gitpush/launch.py
+sudo timedatectl set-ntp true
+sudo timedatectl timesync-status
+
+while ! timeout 0.2 ping -c 1 -n 192.168.1.101 
+do
+	sleep 1
+done
+
+sudo python3 /home/sloth/esp32rpi_serialcomms_gitpush/launch.py >> /home/sloth/esp32rpi_serialcomms_gitpush/syslog.txt
+
